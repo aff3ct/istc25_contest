b24b663db1937fab0ec608906eae9dbd7daadad4
Mathieu Léonardon <mathieu.leonardon@imt-atlantique.fr>
2025-04-16 10:37:45 +0200

    Add pointer prototypes to polar functions.

--- source/decoders/polar_functions.hpp
+++ source/decoders/polar_functions.hpp
@@ -1,9 +1,27 @@
+#ifndef POLAR_FUNCTIONS_HPP_
+#define POLAR_FUNCTIONS_HPP_
+
 template <typename B, typename R>
 B sgn(R val)
 {
 	return (B)((R(0) < val) - (val < R(0)));
 }
 
+
+template <int N_ELMTS>
+inline static void f(const float* l_a,
+       const float* l_b,
+       float* l_c,
+       int n_elmts)
+{
+    for (auto i = 0; i < N_ELMTS; i++)
+    {
+        auto sign_lambda_a_b = sgn<int, float>(l_a[i] * l_b[i]);
+        auto abs_lambda_a = fabsf(l_a[i]);
+        auto abs_lambda_b = fabsf(l_b[i]);
+        l_c[i] = (float)sign_lambda_a_b * std::min(abs_lambda_a, abs_lambda_b);
+    }
+}
 
 template <int N_ELMTS>
 inline static void f(std::vector<float>& l,
@@ -16,13 +34,19 @@ inline static void f(std::vector<float>& l,
     const float* l_b = l.data() + off_l_b;
     float* l_c = l.data() + off_l_c;
 
+    f<N_ELMTS>(l_a, l_b, l_c, n_elmts);
+}
+
+
+template <int N_ELMTS>
+inline static void g(const float* l_a,
+        const float* l_b,
+        const int* s_a,
+        float* l_c,
+        int n_elmts)
+{
     for (auto i = 0; i < N_ELMTS; i++)
-    {
-        auto sign_lambda_a_b = sgn<int, float>(l_a[i] * l_b[i]);
-        auto abs_lambda_a = fabsf(l_a[i]);
-        auto abs_lambda_b = fabsf(l_b[i]);
-        l_c[i] = (float)sign_lambda_a_b * std::min(abs_lambda_a, abs_lambda_b);
-    }
+        l_c[i] = ((s_a[i] == 0) ? l_a[i] : -l_a[i]) + l_b[i];
 }
 
 template <int N_ELMTS>
@@ -39,8 +63,17 @@ inline static void g(std::vector<int>& s,
     const int* s_a = s.data() + off_s_a;
     float* l_c = l.data() + off_l_c;
 
+    g<N_ELMTS>(l_a, l_b, s_a, l_c, n_elmts);
+}
+
+template <int N_ELMTS>
+inline static void g0(const float* l_a,
+        const float* l_b,
+        float* l_c,
+        int n_elmts)
+{
     for (auto i = 0; i < N_ELMTS; i++)
-        l_c[i] = ((s_a[i] == 0) ? l_a[i] : -l_a[i]) + l_b[i];
+        l_c[i] = l_a[i] + l_b[i];
 }
 
 template <int N_ELMTS>
@@ -54,9 +87,20 @@ inline static void g0(std::vector<float>& l,
     const float* l_b = l.data() + off_l_b;
     float* l_c = l.data() + off_l_c;
 
-    for (auto i = 0; i < N_ELMTS; i++)
-        l_c[i] = l_a[i] + l_b[i];
+    g0<N_ELMTS>(l_a, l_b, l_c, n_elmts);
+}
 
+template <int N_ELMTS>
+inline static void gr(const float* l_a,
+        const float* l_b,
+        const int* s_a,
+        float* l_c,
+        int n_elmts)
+{
+    const auto u = s_a[0];
+
+    for (auto i = 0; i < N_ELMTS; i++)
+        l_c[i] = ((u == 0) ? l_a[i] : -l_a[i]) + l_b[i];
 }
 
 template <int N_ELMTS>
@@ -72,10 +116,17 @@ inline static void gr(std::vector<int>& s,
     const float* l_b = l.data() + off_l_b;
     const int* s_a = s.data() + off_s_a;
     float* l_c = l.data() + off_l_c;
-    const auto u = s_a[0];
 
+    gr<N_ELMTS>(l_a, l_b, s_a, l_c, n_elmts);
+}
+
+template <int N_ELMTS>
+inline static void h(const float* l_a,
+        int* s_a,
+        int n_elmts)
+{
     for (auto i = 0; i < N_ELMTS; i++)
-        l_c[i] = ((u == 0) ? l_a[i] : -l_a[i]) + l_b[i];
+        s_a[i] = l_a[i] < 0;
 }
 
 template <int N_ELMTS>
@@ -88,8 +139,7 @@ inline static void h(std::vector<int>& s,
     const float* l_a = l.data() + off_l_a;
     int* s_a = s.data() + off_s_a;
 
-    for (auto i = 0; i < N_ELMTS; i++)
-        s_a[i] = l_a[i] < 0;
+    h<N_ELMTS>(l_a, s_a, n_elmts);
 }
 
 template <int N_ELMTS>
@@ -97,7 +147,16 @@ inline static void h0(std::vector<int>& s,
         const int off_s_a,
         int n_elmts)
 {
-    std::fill(s.begin() + off_s_a, s.begin() + off_s_a + n_elmts, 0);
+    int* s_a = s.data() + off_s_a;
+
+    h0<N_ELMTS>(s_a, n_elmts);
+}
+
+template <int N_ELMTS>
+inline static void h0(const int* s_a,
+        int n_elmts)
+{
+    std::fill(s_a, s_a + n_elmts, 0);
 }
 
 template <int N_ELMTS>
@@ -153,6 +212,16 @@ inline static bool spc(std::vector<int>& s,
 }
 
 template <int N_ELMTS>
+inline static void xo(const int* s_a,
+        const int* s_b,
+        int* s_c,
+        int n_elmts)
+{
+    for (auto i = 0; i < N_ELMTS; i++)
+        s_c[i] = s_a[i] ^ s_b[i];
+}
+
+template <int N_ELMTS>
 inline static void xo(std::vector<int>& s,
         const int off_s_a,
         const int off_s_b,
@@ -163,8 +232,16 @@ inline static void xo(std::vector<int>& s,
     const int* s_b = s.data() + off_s_b;
     int* s_c = s.data() + off_s_c;
 
+    xo<N_ELMTS>(s_a, s_b, s_c, n_elmts);
+}
+
+template <int N_ELMTS>
+inline static void xo0(const int* s_b,
+        int* s_c,
+        int n_elmts)
+{
     for (auto i = 0; i < N_ELMTS; i++)
-        s_c[i] = s_a[i] ^ s_b[i];
+        s_c[i] = s_b[i];
 }
 
 template <int N_ELMTS>
@@ -176,6 +253,7 @@ inline static void xo0(std::vector<int>& s,
     const int* s_b = s.data() + off_s_b;
     int* s_c = s.data() + off_s_c;
 
-    std::copy(s_b, s_b + N_ELMTS, s_c);
+    xo0<N_ELMTS>(s_b, s_c, n_elmts);
 }
 
+#endif // POLAR_FUNCTIONS_HPP_
